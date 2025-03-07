4c2b983d093778d9a1c7567236badbc8ad2f43ba
Jehangir Shah <53529731+jehangir1@users.noreply.github.com>
2025-03-07 23:29:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple Array with 5 fruit names
+ 
+let mixedArray = ["Apple", "Guava", "Banana", "orange", "Berry"];
+mixedArray[2] = "Mango";
+alert("The third element of the Array is replaced by" + " " + mixedArray[2] + ".Please check the Console Log.");
+console.log(mixedArray);
